6d0baddcd8c2aae6034bd78e178847bda6ab0412
Camilo <cetacue@misena.edu.co>
2024-05-22 17:38:44 -0500

    token de acceso concedido,osea login realizado

--- handler.js
+++ handler.js
@@ -1,83 +1,163 @@
-'use strict';
-const AWS = require('aws-sdk');
+"use strict";
+const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const sns = new AWS.SNS(); 
-const { v4: uuidv4 } = require('uuid');
+const sns = new AWS.SNS();
+const { v4: uuidv4 } = require("uuid");
+const ssm = new AWS.SSM();
+const cognitoProvider = new AWS.CognitoIdentityServiceProvider();
 
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Bienvenido a mi API',
+      message: "Bienvenido a mi API",
     }),
   };
 
   return callback(null, response);
 };
 
-// Métodos de mi API------------------------------------------------
+module.exports.authorize = async (event, context, callback) => {
+  const token = event.headers.Authorization;
+  const tokenDev=process.env.TOKEN_DEV
+  if (!token) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: "Token de autorización no proporcionado" }),
+    };
+  }
+  if (
+    token !== tokenDev) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: "Token de autorización inválido" }),
+    };
+  }
+  return null;
+};
+//register------------------------------------------------------------------
 module.exports.createUser = async (event, context, callback) => {
   const body = JSON.parse(event.body);
-  const id = uuidv4();
-  const params = {
-    TableName: 'UsersTable',
-    Item: {
-      id: id,
-      correo: body.correo,
-      contraseña: body.contraseña
-    }
-  };
-
   try {
-    await dynamodb.put(params).promise();
-    await module.exports.snsRequest(`Nuevo usuario creado: ${body.correo}`); 
+    const id = uuidv4();
+    const dynamoParams = {
+      TableName: "UsersTable",
+      Item: {
+        id: id,
+        correo: body.correo,
+        password: body.contraseña,
+      },
+    };
+    await dynamodb.put(dynamoParams).promise();
+
+    const cognitoParams = {
+      UserPoolId: "us-east-1_fz9ot54FN", 
+      Username: body.correo,
+      Password: body.contraseña
+    };
+    const cognitoResponse = await cognitoProvider.adminCreateUser(cognitoParams).promise();
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Usuario creado exitosamente'
-      })
+        message: "Usuario creado correctamente. Por favor, inicia sesión y cambia tu contraseña para continuar",
+        userId: id,
+      }),
     };
   } catch (error) {
-    console.error('Error al crear usuario', error);
+    console.error("Error al crear usuario", error);
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: 'Ocurrió un error al crear usuario'
-      })
+        error: "Ocurrió un error al crear usuario",
+      }),
     };
   }
 };
 
+//login-------------------------------------------------------------
+module.exports.login = async (event, context, callback) => {
+  const body = JSON.parse(event.body);
+  try {
+    const authParams = {
+      AuthFlow: "ADMIN_USER_PASSWORD_AUTH",
+      UserPoolId: "us-east-1_fz9ot54FN",
+      ClientId: "19vtl6pqe6detnupu4vrtde3i2",
+      AuthParameters: {
+        USERNAME: body.correo,
+        PASSWORD: body.contraseña,
+      }
+    };
+    const authResponse = await cognitoProvider.adminInitiateAuth(authParams).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Inicio de sesión exitoso",
+        accessToken: authResponse.AuthenticationResult.AccessToken,
+      }),
+    };
+  } catch (error) {
+    console.error("Error al iniciar sesión", error);
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        error: "Credenciales inválidas. Por favor, verifica tu usuario y contraseña e intenta nuevamente.",error
+      }),
+    };
+  }
+};
+
+
+
+
+// Métodos de mi API------------------------------------------------
 module.exports.getAllUsers = async (event, context, callback) => {
+  const authorizationResult = await module.exports.authorize(
+    event,
+    context,
+    callback
+  );
+  if (authorizationResult) {
+    return authorizationResult;
+  }
+
   const params = {
-    TableName: 'UsersTable'
+    TableName: "UsersTable",
   };
 
   try {
     const result = await dynamodb.scan(params).promise();
     return {
       statusCode: 200,
-      body: JSON.stringify(result.Items)
+      body: JSON.stringify(result.Items),
     };
   } catch (error) {
-    console.error('Error al obtener usuarios', error);
+    console.error("Error al obtener usuarios", error);
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: 'Ocurrió un error al obtener usuarios'
-      })
+        error: "Ocurrió un error al obtener usuarios",
+      }),
     };
   }
 };
 
 module.exports.getUserById = async (event, context, callback) => {
+  const authorizationResult = await module.exports.authorize(
+    event,
+    context,
+    callback
+  );
+  if (authorizationResult) {
+    return authorizationResult;
+  }
+
   const { id } = event.pathParameters;
 
   const params = {
-    TableName: 'UsersTable',
+    TableName: "UsersTable",
     Key: {
-      id: id
-    }
+      id: id,
+    },
   };
 
   try {
@@ -85,33 +165,42 @@ module.exports.getUserById = async (event, context, callback) => {
     if (result.Item) {
       return {
         statusCode: 200,
-        body: JSON.stringify(result.Item)
+        body: JSON.stringify(result.Item),
       };
     } else {
       return {
         statusCode: 404,
-        body: JSON.stringify({ error: 'Usuario no encontrado' })
+        body: JSON.stringify({ error: "Usuario no encontrado" }),
       };
     }
   } catch (error) {
-    console.error('Error al obtener usuario', error);
+    console.error("Error al obtener usuario", error);
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: 'Ocurrió un error al obtener usuario'
-      })
+        error: "Ocurrió un error al obtener usuario",
+      }),
     };
   }
 };
 
 module.exports.deleteUser = async (event, context, callback) => {
+  const authorizationResult = await module.exports.authorize(
+    event,
+    context,
+    callback
+  );
+  if (authorizationResult) {
+    return authorizationResult;
+  }
+
   const { id } = event.pathParameters;
 
   const params = {
-    TableName: 'UsersTable',
+    TableName: "UsersTable",
     Key: {
-      id: id
-    }
+      id: id,
+    },
   };
 
   try {
@@ -119,35 +208,44 @@ module.exports.deleteUser = async (event, context, callback) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Usuario eliminado exitosamente'
-      })
+        message: "Usuario eliminado exitosamente",
+      }),
     };
   } catch (error) {
-    console.error('Error al eliminar usuario', error);
+    console.error("Error al eliminar usuario", error);
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: 'Ocurrió un error al eliminar usuario'
-      })
+        error: "Ocurrió un error al eliminar usuario",
+      }),
     };
   }
 };
 
 module.exports.updateUser = async (event, context, callback) => {
+  const authorizationResult = await module.exports.authorize(
+    event,
+    context,
+    callback
+  );
+  if (authorizationResult) {
+    return authorizationResult;
+  }
+
   const { id } = event.pathParameters;
   const body = JSON.parse(event.body);
 
   const params = {
-    TableName: 'UsersTable',
+    TableName: "UsersTable",
     Key: {
-      id: id
+      id: id,
     },
-    UpdateExpression: 'set correo = :correo, contraseña = :contraseña',
+    UpdateExpression: "set correo = :correo, contraseña = :contraseña",
     ExpressionAttributeValues: {
-      ':correo': body.correo,
-      ':contraseña': body.contraseña
+      ":correo": body.correo,
+      ":contraseña": body.contraseña,
     },
-    ReturnValues: 'UPDATED_NEW'
+    ReturnValues: "UPDATED_NEW",
   };
 
   try {
@@ -155,33 +253,43 @@ module.exports.updateUser = async (event, context, callback) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Usuario actualizado exitosamente',
-        updatedAttributes: result.Attributes
-      })
+        message: "Usuario actualizado exitosamente",
+        updatedAttributes: result.Attributes,
+      }),
     };
   } catch (error) {
-    console.error('Error al actualizar usuario', error);
+    console.error("Error al actualizar usuario", error);
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: 'Ocurrió un error al actualizar usuario'
-      })
+        error: "Ocurrió un error al actualizar usuario",
+      }),
     };
   }
 };
 
 // SNS------------------------------------------------
-module.exports.snsRequest = async (mensaje) => {
-  let arn = process.env.SNS_ARN
-  const params = {
-    Message: mensaje,
-    TopicArn: arn 
-  };
 
+module.exports.snsRequest = async (event) => {
   try {
+    const mensaje = JSON.parse(event.body).mensaje;
+    const arn = process.env.SNS_ARN;
+    const params = {
+      Message: mensaje,
+      TopicArn: arn,
+    };
     await sns.publish(params).promise();
-    console.log('Mensaje enviado a SNS');
+    console.log("Mensaje enviado a SNS");
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Mensaje enviado a SNS correctamente",event })
+      
+    };
   } catch (error) {
-    console.error('Error al enviar mensaje a SNS', error);
+    console.error("Error al enviar mensaje a SNS", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Error al enviar mensaje a SNS" })
+    };
   }
 };
